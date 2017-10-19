640156a12017fb2d7edce5ba139078499770e6f8
SheaClose <SheaClose@gmail.com>
2017-10-18 20:25:03 -0500

    done

--- src/ducks/actions.js
+++ src/ducks/actions.js
@@ -17,73 +17,39 @@ import {
   EMAIL
 } from './action_types';
 
-function actionCreator(payload, type) {
-  return {
-    payload,
-    type
-  };
-}
+const action = (payload, type) => ({ payload, type });
+const updateLoanType = payload => action(payload, UPDATE_LOAN_TYPE);
+const updatePropertyType = payload => action(payload, UPDATE_PROPERTY_TYPE);
+const updateCity = payload => action(payload, UPDATE_CITY);
+const propToBeUsedOn = payload => action(payload, PROP_TO_BE_USED_ON);
+const handleFound = payload => action(payload, FOUND);
+const handleRealEstateAgent = payload => action(payload, REAL_ESTATE_AGENT);
+const updateCost = payload => action(payload, COST);
+const updateDownPayment = payload => action(payload, DOWN_PAYMENT);
+const updateCredit = payload => action(payload, CREDIT);
+const updateHistory = payload => action(payload, HISTORY);
+const updateAddLineOne = payload => action(payload, LINE_ONE);
+const updateAddLineTwo = payload => action(payload, LINE_TWO);
+const updateAddLineThree = payload => action(payload, LINE_THREE);
+const updateFirst = payload => action(payload, FIRST);
+const updateLast = payload => action(payload, LAST);
+const updateEmail = payload => action(payload, EMAIL);
 
-export function updateLoanType(payload) {
-  return actionCreator(payload, UPDATE_LOAN_TYPE);
-}
-
-export function updatePropertyType(payload) {
-  return actionCreator(payload, UPDATE_PROPERTY_TYPE);
-}
-
-export function updateCity(payload) {
-  return actionCreator(payload, UPDATE_CITY);
-}
-
-export function propToBeUsedOn(payload) {
-  return actionCreator(payload, PROP_TO_BE_USED_ON);
-}
-
-export function handleFound(payload) {
-  return actionCreator(payload, FOUND);
-}
-
-export function handleRealEstateAgent(payload) {
-  return actionCreator(payload, REAL_ESTATE_AGENT);
-}
-
-export function updateCost(payload) {
-  return actionCreator(payload, COST);
-}
-
-export function updateDownPayment(payload) {
-  return actionCreator(payload, DOWN_PAYMENT);
-}
-
-export function updateCredit(payload) {
-  return actionCreator(payload, CREDIT);
-}
-
-export function updateHistory(payload) {
-  return actionCreator(payload, HISTORY);
-}
-
-export function updateAddLineOne(payload) {
-  return actionCreator(payload, LINE_ONE);
-}
-
-export function updateAddLineTwo(payload) {
-  return actionCreator(payload, LINE_TWO);
-}
-
-export function updateAddLineThree(payload) {
-  return actionCreator(payload, LINE_THREE);
-}
-
-export function updateFirst(payload) {
-  return actionCreator(payload, FIRST);
-}
-
-export function updateLast(payload) {
-  return actionCreator(payload, LAST);
-}
-
-export function updateEmail(payload) {
-  return actionCreator(payload, EMAIL);
-}
+export {
+  updateLoanType,
+  updatePropertyType,
+  updateCity,
+  propToBeUsedOn,
+  handleFound,
+  handleRealEstateAgent,
+  updateCost,
+  updateDownPayment,
+  updateCredit,
+  updateHistory,
+  updateAddLineOne,
+  updateAddLineTwo,
+  updateAddLineThree,
+  updateFirst,
+  updateLast,
+  updateEmail
+};
